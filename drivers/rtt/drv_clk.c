b5e36f8f85440e5bb5be6f11962637d541247764
luobeihai <571688853@qq.com>
2023-03-06 16:10:37 +0800

    copy from v1.0.0

---
+++ drivers/rtt/drv_clk.c
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2006-2022, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author        Notes
+ * 2022-07-15     Aligagago     first version
+ */
+
+#include "apm32f4xx.h"
+
+void clk_init(char *clk_source, int source_freq, int target_freq)
+{
+    SystemInit();
+}
